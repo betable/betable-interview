59158f67edb94f0b69faad2eb42dc98b6c139515
Robert Fruchtman <rfruchtose@gmail.com>
2015-10-20 16:05:07 -0700

    Render index.html

--- app/index.js
+++ app/index.js
@@ -1,5 +1,9 @@
 'use strict';
 
+var path = require('path');
+
+var root = path.join(__dirname, '..');
+
 module.exports = function index(req, res) {
-	res.send({ response: 'Hello.' });
+	res.sendFile(path.join(root, 'templates', 'index.html'));
 };
