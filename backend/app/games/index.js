ac20fa47cd16c74981d9895f677d7176405994d6
Robert Fruchtman <rfruchtose@gmail.com>
2015-10-21 11:56:23 -0700

    Add games list endpoint and define middleware functions outside of the routes function

--- backend/app/games/index.js
+++ backend/app/games/index.js
@@ -3,12 +3,17 @@
 var express = require('express');
 
 var sessionVerification = require('../sessions/middleware').sessionVerification,
+    querySession = sessionVerification('query.session_id'),
+    bodySession = sessionVerification('body.session_id');
+
+var list = require('./list'),
     suggested = require('./suggested'),
     play = require('./play');
 
 module.exports = function routes() {
     var router = express.Router();
-    router.get('/suggested', sessionVerification('query.session_id'), suggested);
-    router.post('/:id/play', sessionVerification('body.session_id'), play);
+    router.get('/', querySession, list);
+    router.get('/suggested', querySession, suggested);
+    router.post('/:id/play', bodySession, play);
     return router;
 };
