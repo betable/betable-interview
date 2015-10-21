807eeb0748f3f5a24dd022a46fbfef75bc666400
Robert Fruchtman <rfruchtose@gmail.com>
2015-10-21 11:51:26 -0700

    Add game data store with JSON source

--- backend/app/games/play.js
+++ backend/app/games/play.js
@@ -2,11 +2,19 @@
 
 var request = require('request');
 
-var config = require('../../config');
+var config = require('../../config'),
+    games = require('./store');
 
 module.exports = function play (req, res) {
     var gameId = req.params.id,
         sessionId = req.body.session_id;
+
+    if (!games.index[gameId]) {
+        return res.status(404).send({
+            error: 'INVALID_GAME',
+            description: 'The game for this request is missing, or is invalid'
+        });
+    }
 
     request.post('http://localhost:' + config.PORT + '/hook', {
         event: 'play',
@@ -15,6 +23,6 @@ module.exports = function play (req, res) {
         },
         session_id: sessionId
     }, function() {
-        res.status(200).send({status: 'played'});
+        res.status(200).send({success: true});
     })
 };
--- backend/app/games/suggested.js
+++ backend/app/games/suggested.js
@@ -1,5 +1,11 @@
 'use strict';
 
+var _ = require('lodash');
+
+var games = require('./store');
+
 module.exports = function suggested(req, res) {
-    res.status(200).send([]);
+    res.status(200).send({
+        games: _.sample(games.list, 5)
+    });
 };
