c4f7c8f8355e3f3712134dc260c53d5fd8719ace
Robert Fruchtman <rfruchtose@gmail.com>
2015-10-20 19:16:40 -0700

    ID guaranteed to be unique

--- backend/app/sessions/create.js
+++ backend/app/sessions/create.js
@@ -4,14 +4,30 @@ var crypto = require('crypto');
 
 var store = require('./store');
 
+// http://stackoverflow.com/a/21963136
+var uuid = (function() {
+  var lut = new Array(256);
+  for (var i=0; i<256; i++) { lut[i] = (i<16?'0':'')+(i).toString(16); }
+  return function generate() {
+    var d0 = Math.random()*0xffffffff|0;
+    var d1 = Math.random()*0xffffffff|0;
+    var d2 = Math.random()*0xffffffff|0;
+    var d3 = Math.random()*0xffffffff|0;
+    return lut[d0&0xff]+lut[d0>>8&0xff]+lut[d0>>16&0xff]+lut[d0>>24&0xff]+'-'+
+      lut[d1&0xff]+lut[d1>>8&0xff]+'-'+lut[d1>>16&0x0f|0x40]+lut[d1>>24&0xff]+'-'+
+      lut[d2&0x3f|0x80]+lut[d2>>8&0xff]+'-'+lut[d2>>16&0xff]+lut[d2>>24&0xff]+
+      lut[d3&0xff]+lut[d3>>8&0xff]+lut[d3>>16&0xff]+lut[d3>>24&0xff];
+  };
+})();
+
 function create(username) {
     var sha256 = crypto.createHash('sha256');
-    sha256.update(username, 'utf8');
+    sha256.update(username + uuid(), 'utf8');
     var id = sha256.digest('base64');
     var record = {
-        user_name: username
+        session_id: id
     };
-    store.set(id, record);
+    store.set(username, record);
     return id;
 }
 
